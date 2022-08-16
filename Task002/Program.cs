d7143cd78d9c70128a92d2fcf4c0692067af5b09
Maratius <maratius.ru@gmail.com>
2022-08-16 20:57:26 +0300

    123

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -3,12 +3,16 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-Console.WriteLine("Введите координаты x, y, z первой точки, x = ");
+Console.WriteLine("Введите координаты первой точки");
+
+Console.WriteLine("x = ");
 double x1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("y = ");
 double y1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("z = ");
 double z1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
 
 Console.WriteLine("Введите координаты x, y, z второй точки, x = ");
 double x2 = Convert.ToDouble(Console.ReadLine());
